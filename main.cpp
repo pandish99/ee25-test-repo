f3c7371c685d4e2f771cdf709fd4c1733244c6fa
Tim Tun <timtun99@gmail.com>
2026-09-09 14:45:23 +0200

    Format code

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
  * @file Firmware demo.
  */
 
- #include <cstdio>
+#include <cstdio>
 
- /**
-  * @brief Application entry point.
-  * 
-  * @return 0 on termination of the program.
-  */
- int main()
- {
+/**
+ * @brief Application entry point.
+ *
+ * @return 0 on termination of the program.
+ */
+int main()
+{
     std::printf("Hello, I'm your firmware!\n");
     return 0;
- }
+}
